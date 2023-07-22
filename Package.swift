d58c3bdd0cc9d8f385f294bddb0c00e2d755692c
NickLand <nick.rango.land@gmail.com>
2023-07-22 23:13:45 +0800

    排除示例文件夹

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .target(
             name: "LandKit",
-            dependencies: []),
+            dependencies: [],
+            exclude:["Example/"]),
         .testTarget(
             name: "LandKitTests",
             dependencies: ["LandKit"]),
